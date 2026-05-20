953d18ad25207896debd4ecf88dc3400d47bcacf
rashedmojammel <rashedmojamme56@gmail.com>
2026-05-20 18:20:14 +0600

    fix the issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,28 +7,54 @@ import { jwt } from "better-auth/plugins";
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("pet_adoption_platform");
 
+// export const auth = betterAuth({
+
+//   secret: process.env.BETTER_AUTH_SECRET,
+//   database: mongodbAdapter(db),
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+//   socialProviders: {
+//     google: {
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     },
+//   },
+//   session: {
+//     cookieCache : {
+//       enabled: true,
+//       strategy : "jwt",
+//       maxAge: 60 * 60 * 24 * 7, // 7 days
+//     }
+//   },
+//   plugins: [
+
+//     jwt()
+
+//   ]
+// });
 export const auth = betterAuth({
-  secret: process.env.BETTER_AUTH_SECRET,
-  database: mongodbAdapter(db),
+  database: mongodbAdapter(db, {
+    client
+  }),
   emailAndPassword: {
     enabled: true,
   },
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
+         clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
+    }
   },
-  session: {
-    cookieCache : {
+  session : {
+    cookieCache: {
       enabled: true,
-      strategy : "jwt",
-      maxAge: 60 * 60 * 24 * 7, // 7 days
+      strategy: "jwt",
+      //max 7days
+      maxAge: 7 * 24 * 60 * 60
     }
   },
   plugins: [
-
     jwt()
-
   ]
 });
